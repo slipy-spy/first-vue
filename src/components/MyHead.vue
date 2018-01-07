aeee66fd05afcd79f09f5398ca6763acdf1d4f8b
shipy <458898434@qq.com>
2018-01-08 00:04:43 +0800

    new vue file

---
+++ src/components/MyHead.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>i am the head</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "my-head"
+    }
+</script>
+
+<style scoped>
+
+</style>
