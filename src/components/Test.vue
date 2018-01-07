a443d4538680fcaa1b0a76cb792586f13b33f978
shipy <458898434@qq.com>
2018-01-07 22:07:40 +0800

    new vue file

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <button>返回</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test"
+    , data() {
+      return {
+        msg: 'fuck'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
